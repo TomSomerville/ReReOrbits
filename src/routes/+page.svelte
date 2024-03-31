fbda0bc76fe8faba7829164f954c71f41eb39dbb
thomas.somerv@gmail.com <thomas.somerv@gmail.com>
2024-03-31 02:15:34 -0400

    ReReOrbits Copy Over

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { draggable } from '@neodrag/svelte';
+	import Czone from './Czone.svelte';
+</script>
+<style>
+.banner {
+		width:600px;
+		height:100px;
+		display: inline-block;
+		background-image: url(https://i.imgur.com/Qk77nuu.png);
+		background-size: 600px 175px;
+		background-position: center;
+	}
+	.re-orbit_logo {
+		width:330px;
+		height:100px;
+		display: inline-block;
+		background-image: url(https://i.imgur.com/QgTETB0.png);
+		background-size: 330px;		
+	}
+	.top_bar {
+		display: inline-block;
+		white-space: nowrap;
+		padding: 5px 5px 5px 5px;
+		position: center;
+		margin-bottom: 5px;
+		
+	}
+</style>
+
+<div class="top_bar">
+	<span>
+	<div class="re-orbit_logo"></div>
+	<div class="banner"></div>
+</span>
+</div>
+
+<Czone />
